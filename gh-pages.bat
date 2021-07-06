68cc302a1fd9b3c518c3f2f5ce2d5c7ce37a4104
Haridev <haridevgireesh@gmail.com>
2021-07-07 01:43:07 +0530

    Bug Fix in route/index.js

--- gh-pages.bat
+++ gh-pages.bat
@@ -1,2 +1,1 @@
-#!/bin/sh
 git subtree push --prefix dist origin gh-pages
--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,7 @@ const routes = [
       if (currentUser) {
         return '/chats'
       }
-      return '/users'
+      return '/login'
     }
   },
   {
